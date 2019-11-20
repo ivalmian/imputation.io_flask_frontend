f50cb0347b8453aa40b70be5707342e4177b57ed
ivalmian <ivalmian@gmail.com>
2019-11-19 19:13:07 -0800

    fixed csrf key, still didn't fix csrf based errors

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 DEBUG = False  # Turns on debugging features in Flask
 
-import os
-SECRET_KEY = os.urandom(32)
+# import os
+SECRET_KEY = b'daae7948824525c1b8b59f9d5a75e9c0404e46259c7b1e17a4654a7e73c91b87'
 FLASK_ENV = "prod"
 
 PROJECT = 'census-impute'
