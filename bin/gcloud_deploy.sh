01b3d291633d520a06252fc65a996b4c0c4cf531
ilya <ivalmian@gmail.com>
2020-06-10 00:01:17 -0700

    bin/gcloud_deploy.sh now run tox before deploying and doesn't deploy if we error out

--- bin/gcloud_deploy.sh
+++ bin/gcloud_deploy.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-#assumes you have gcloud properly authenticated and appengine-clean setup
+# Assumes you have gcloud properly authenticated and appengine-clean setup
 
-appengine-clean census-impute 5 --force
-gcloud app deploy app.yaml
+# Steps:
+# 1. Run test suite with tox
+# 2. If tests ok:
+#    a. Delete all but the last 5 version numbers
+#    b. Deploy the app to google apps engine
 
+tox
+retVal=$?
+
+if [ $retVal -eq 0 ]; then
+    appengine-clean census-impute 5 --force
+    gcloud app deploy app.yaml
+else
+    echo "Cancelling deployment since tox returned error code $retVal"
+fi
+
+exit $retVal
