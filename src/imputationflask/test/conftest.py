842e1d7d570b9367ab947ba3639cd6e6bccd73f2
ilya <ivalmian@gmail.com>
2020-06-17 21:09:26 -0700

    reworked conftest, changed form_data to be a parametrized fixture

---
+++ src/imputationflask/test/conftest.py
@@ -0,0 +1,43 @@
+'''
+imputationflask.test.conftest
+-------------------
+Utilities and configuration for testing
+'''
+
+import pytest
+from imputationflask import app#, db
+from numpy.random import choice
+from imputationflask import data_dictionary, binaries_dict
+
+NUM_FUZZY_TRIES = 10 #how many times to try random input
+
+@pytest.fixture(params=range(NUM_FUZZY_TRIES))
+def form_data(request):
+    
+    data = dict()
+
+    for key in binaries_dict['numeric_mappers'].keys():
+
+        data[key]=str(choice(list(range(100))))
+        data['mask_' + key]=choice(['y','n'])
+
+    for key in data_dictionary.data_dict.keys():
+
+        data[key]=choice([-1,0,1])
+        data['mask_' + key]=choice(['y','n'])
+
+    return data
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    
+    with app.test_client() as client:
+        # with app.app_context():
+        #     db.create_all()
+        yield client
+
+#    db.drop_all()
+
+
