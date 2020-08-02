0d3e3d592a56199ede0b2f5cb3715680742a2d9d
ilya <ivalmian@gmail.com>
2020-08-01 17:27:17 -0700

    chore: refactored test code from the src

--- bin/run_local.sh
+++ bin/run_local.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 
-export GOOGLE_APPLICATION_CREDENTIALS="../gcloud_credential/census-impute-d47bc435f1d0.json"
-gunicorn --chdir src "imputationflask:application()" -b :5000
+export GOOGLE_APPLICATION_CREDENTIALS="gcloud_credential/census-impute-d47bc435f1d0.json"
+export PYTHONPATH="src"
+gunicorn "imputationflask:application()" -b :5000
